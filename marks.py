ed65db87c2e6766b0504d90c12bdb615aa853b6d
shanmugapandiyan <shanmugapandiyan927@gmail.com>
2026-07-16 18:43:54 +0530

    Added README images

--- marks.py
+++ marks.py
@@ -1,31 +1,29 @@
-username= input("Enter user name: ")
+def percentage(option):
+    if option == "cc":
+        maths = int(input("Enter Maths mark: "))
+        physics = int(input("Enter Physics mark: "))
+        chemistry = int(input("Enter Chemistry mark: "))
+
+        total = physics /2 + chemistry /2
+        display=total + maths 
+        print("calculate of marks:",display)
+        
+    elif option == "mp":
+        subject=(input("enter subject name:"))
+        
+        if subject in ["maths","physics","chemistry"]:
+            mark=int(input("enter a mark:"))
+            value = mark / 10
+        print(value)
+            
+    else:
+        print("Invalid Option")
+
+username = input("Enter user name: ")
 Dob = input("Enter DOB: ")
 
 print("Option -- [calculate -> cc, mark to percentage -> mp]")
 
 option = input("Enter option: ")
 
-if option == "cc":
-    maths = int(input("Enter Maths: "))
-    physics = int(input("Enter Physics: "))
-    chemistry = int(input("Enter Chemistry: "))
-
-    total = (maths + physics + chemistry) / 2  
-    print("Calculate of marks:", total)
-
-elif option == "mp":
-    subject = input("Enter Subject Name: ")
-
-    if subject in ["maths", "physics", "chemistry"]:
-        marks = int(input("Enter Mark: "))
-        percentage = marks / 10
-
-print("Percentage:", percentage)
-        
-        
-
-
-
- 
-
-
+percentage(option)
